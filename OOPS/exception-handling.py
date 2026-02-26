cbfb74d70273940c6fda1935af5df4a8d960b0f7
Abey John Pramod <146655666+abeyjohnp@users.noreply.github.com>
2026-02-26 09:44:42 +0530

    Update exception-handling.py

--- OOPS/exception-handling.py
+++ OOPS/exception-handling.py
@@ -17,10 +17,40 @@ ExceptionClasse:
     IndexError (Lookup error)
     ZeroDivisionError (Arithmetic Error)
 """
-
+#1
 try:
     n=10
     d=0
     print(n/d)
-except ZeroDivisionError:
+except ZeroDivisionError: 
     print("Error, Division by 0")
+#2
+try:
+    print(x)
+except NameError: 
+    print("No Name")
+except:#this will work when we dont know which exception, so it works for all exception!
+    print("Some exception occured!")
+
+#3
+try:
+    print("Hello")
+except:
+    print("Some wrong!")
+else: #if no exception
+    print("Nothing went wrong")
+
+#4
+try:
+    print("Hello")
+except:
+    print("Some wrong!")
+else:
+    print("Else!")
+finally:
+    print("Nothing went wrong - Finished Try Except")
+
+#if at all no exception handled, it is handled by runtime! Finally will however print even if an error has occured,
+#so if at all error occured, it will check finally, print that and then runtime error occurs.
+
+
