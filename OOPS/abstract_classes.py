396c6e3243e6036f25587f592680d0da1f2dc3dd
Abey John Pramod <146655666+abeyjohnp@users.noreply.github.com>
2026-02-26 09:31:51 +0530

    Update abstract_classes.py

---
+++ OOPS/abstract_classes.py
@@ -0,0 +1,14 @@
+"""
+a class that can contain abstract method, no definition, only function header.
+if a class consist of abstract method, it is called abstract class.
+it can have concrete methods.
+to write an abstract class in python, import ABC class from ABC module.
+from abc import ABC.abstractmethod
+    #whenever u write a class subclass of ABC, that class becomes abstract class.
+    abstract method - in java it is abstract void fn()
+    here - @abstractmethod
+        #inside function, since no definition, give pass
+    abstract class definition implemeneted by child class
+    
+"""
+
