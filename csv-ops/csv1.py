4ce6ffd614927e2addee58cb1e89ee1a58af6f0f
Abey John Pramod <146655666+abeyjohnp@users.noreply.github.com>
2026-03-25 09:47:13 +0530

    csv ops

--- csv-ops/csv1.py
+++ csv-ops/csv1.py
@@ -1,11 +1,20 @@
 import pandas as pd
 df = pd.read_csv("test.csv")
+print("Total Dataset : ")
 print(df)
 
-print(df["Name"].sort_values())
+n=7
+print("First ",n,"records")
+print(df.head(n))
 
-print(df.loc[df["Mark"].idxmax(),"Name"])
 
-print(df.loc[df["Gender"]=="M","Name"])
+#print(df["Name"].sort_values())
 
-print(df["Department"].unique())
+print("Name of topper : ")
+print(df.loc[df["Mark"].idxmax(),"Name"]) #idx returns the index number of the topper, and based on that we need to get the Name
+
+print("Females : ")
+print(df.loc[df["Gender"]=="F","Name"]) #df.loc is used to retreive the value from the dataframe. 
+
+print("Departments : ")
+print(df["Department"].unique()) 
