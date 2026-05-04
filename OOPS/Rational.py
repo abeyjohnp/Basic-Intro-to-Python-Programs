3d3f42a21361f29b2b4a0a7631ebf5a9fc132140
Abey John Pramod <146655666+abeyjohnp@users.noreply.github.com>
2026-05-04 07:44:48 +0530

    refactor Rational class initialization and string representation

--- OOPS/Rational.py
+++ OOPS/Rational.py
@@ -1,25 +1,8 @@
 class Rational:
-    def __init__(self,numer,denom):
-        self.numer=numer
-        self.denom=denom
-        self._reduce()
-    
-    def numerator(self):
-        return self.numer
-    
-    def denominator(self):
-        return self.denom
-    
-    def _reduce(self):
-        divisor=self._gcd(self.numer,self.denom)
-        self.numer=self.numer//divisor
-        self.denom=self.denom//divisor
+    def __init__(self,n,d):
+        self.numer=n
+        self.denom=d
+    def __str__(self):
+        return (self.numer/self.self.denom)
 
-    def gcd(self,a,b):
-
-        if b == 0:
-            return a
-        else:
-            return self.gcd(self,b, a % b)
-
-        
+oneHalf = Rational(2,3)
