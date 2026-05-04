20e464713b06f5137dc10df94aa9fb7b3a226809
Abey John Pramod <146655666+abeyjohnp@users.noreply.github.com>
2026-05-04 07:51:09 +0530

    remove Rational class and its instance

--- OOPS/Rational.py
+++
@@ -1,8 +0,0 @@
-class Rational:
-    def __init__(self,n,d):
-        self.numer=n
-        self.denom=d
-    def __str__(self):
-        return (self.numer/self.self.denom)
-
-oneHalf = Rational(2,3)
